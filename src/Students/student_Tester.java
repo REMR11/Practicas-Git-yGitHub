db6f32e4728d19e23505769ae0301f81782ddbe5
REMR11 <ronaldmejia0211@gmail.com>
2023-10-24 15:47:28 -0600

    Peticion de datos en main, funcional

--- src/Students/student_Tester.java
+++ src/Students/student_Tester.java
@@ -1,11 +1,29 @@
 package Students;
+
 import Students.student;
+import java.util.Scanner;
 
 public class student_Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		student st = new student();
+
+		System.out.println("Proporciona Id de estudiante: ");
+		st.setStudent_Id(Integer.parseInt(sc.nextLine()));
+
+		System.out.println("Proporciona primer nombre de estudiante: ");
+		st.setfName(sc.nextLine());
+
+		System.out.println("Proporciona apellido nombre de estudiante: ");
+		st.setlName(sc.nextLine());
+
+		System.out.println("Proporciona el estado del alumno: activo(1), inactivo(0)");
+		String status_Student = sc.nextLine();
+		st.setStatus_Student(status_Student.equals("1"));
+
+		System.out.println(st.To_String());
 	}
 
 }
